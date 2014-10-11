70faa98fd6a537d346c05a6c666c75cbad3e818f
Nic Young <nryoung@gmail.com>
2014-10-11 12:22:08 -0600

    Fix naive implementation to actually work, just iterates over both lists now. Add efficient sorted implementation. Added test cases for both.

--- 1/1-3.py
+++ 1/1-3.py
@@ -4,24 +4,38 @@ other.
 """
 
 
-def permutation_set(a, b):
+def permutation_naive(a, b):
     """
-    A naive implementation that I came up with utilizing a set. This has worst
+    A naive implementation iterating over. This has worst
     case running time of O(n ** 2).
     """
 
     if len(a) != len(b):
         return False
 
-    chars = set()
+    chars = [c for c in a]
 
-    for c in a:
-        chars.add(c)
+    for char in b:
+        for x in chars:
+            if char == x:
+                chars.remove(x)
+                break
 
-    for c in b:
-        if c in chars:
-            continue
-        else:
+    if chars:
+        return False
+    return True
+
+
+def permutation_sorted(a, b):
+    """
+    Efficient solution that sorts both strings and then compares them. This has
+    a worst running tmie of O(n * log(n))
+    """
+    if len(a) != len(b):
+        return False
+
+    for i, j in zip(sorted(a), sorted(b)):
+        if i != j:
             return False
 
     return True
@@ -29,4 +43,11 @@ def permutation_set(a, b):
 if __name__ == '__main__':
     a = 'aaabbbccc'
     b = 'babacabcc'
-    print("'{}' is a permutation of '{}': {}".format(b, a, permutation_set(a, b)))
+    print("'{}' is a permutation of '{}': {}".format(b, a, permutation_naive(a, b)))
+    print("'{}' is a permutation of '{}': {}".format(b, a, permutation_sorted(a, b)))
+
+    # Test when not a permutation, off by one char
+    c = 'aaaabbbccc'
+    d = 'aaabbbbccc'
+    print("'{}' is a permutation of '{}': {}".format(c, d, permutation_naive(c, d)))
+    print("'{}' is a permutation of '{}': {}".format(c, d, permutation_sorted(c, d)))

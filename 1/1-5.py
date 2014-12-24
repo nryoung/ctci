8917b7bde8a74e490ae73d58681dfff19f390c9e
Nic Young <nryoung@gmail.com>
2014-12-23 17:10:37 -0700

    Remove first length check from function, implement more elegant way to build the string.

--- 1/1-5.py
+++ 1/1-5.py
@@ -3,11 +3,6 @@ Implement a method to perform basic string compression using the counts of
 repeated characters. For example, the string aabcccccaa would become a2b1c5a3.
 If the compressed string would nto become smaller than the original string,
 your method should return the original string.
-
-NOTES:
-* The building of the compressed string is ugly and could probably be improved
-* There is probably a better way to append the last char, count tuple at the
-  end.
 """
 
 
@@ -28,18 +23,11 @@ def compress(s):
             prev_char = c
             count = 1
 
-        # Check if the compression will be larger than original
-        if len(char_counts) >= len(s):
-            return s
-
     # string finished append last bit of data
     char_counts.append((prev_char, count))
 
-    # More elegant way to do this?
-    compressed = ''
-    for char, count in char_counts:
-        compressed += char
-        compressed += str(count)
+    # compress
+    compressed = ''.join('%s%s' % tup for tup in char_counts)
 
     # Check if the compression will be larger than original
     if len(compressed) >= len(s):

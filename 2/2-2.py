b57561556a4c81c3404abcbbc67ee132649b188b
Nic Young <nryoung@gmail.com>
2015-01-06 22:22:21 -0700

    Add recursion solution to ex 2-2

--- 2/2-2.py
+++ 2/2-2.py
@@ -8,20 +8,24 @@ if it were to read: 'Return the kth from the last element'.
 from linked_list import LinkedList
 
 
-def find_k_from_last(head, k, i):
+def find_k_from_last(head, k, i, buf):
     """
-    Uses recursion to find this
+    Uses recursion to find the tail of the linked list. Along the way all of
+    the values are put in to a buffer. Once the tail is found we take length of
+    linked_list - k from the buffer and that will be our value. That is floated
+    up through the call stack. Think of the buffer as a circular buffer
+    (sorta).
+
+    The advantage of using recursion is that it is extremely easy to implement.
+    The disadvantage is that this will eat up O(n) space and O(n) time. If we
+    had a real the circular buffer then the space would be O(k).
     """
     if not head:
-        return None
+        return buf[i-k]
+    buf.append(head.value)
 
-    node = find_k_from_last(head.next, k, i)
-    #XXX
-    i += 1
-    if i == k:
-        return head
-    return node
-
+    value = find_k_from_last(head.next, k, i+1, buf)
+    return value
 
 if __name__ == '__main__':
     linked_list = LinkedList()
@@ -35,5 +39,5 @@ if __name__ == '__main__':
 
     k = 3
     print('Finding k:{} from the last element:{}'.format(
-        k, find_k_from_last(linked_list.head, k, 0).value)
+        k, find_k_from_last(linked_list.head, k, 0, []))
     )
